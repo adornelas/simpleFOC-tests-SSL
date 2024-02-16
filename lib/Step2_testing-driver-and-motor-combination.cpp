78f73025d66ea97438e03dff8b1d5cc6e792a33e
adornelas <andredornelas23@gmail.com>
2024-02-16 19:29:49 -0300

    changes example to the original one on the oficial repo

--- lib/Step2_testing-driver-and-motor-combination.cpp
+++ lib/Step2_testing-driver-and-motor-combination.cpp
@@ -7,24 +7,32 @@ void doPWM(){sensor.handlePWM();}
 BLDCMotor motor = BLDCMotor(11);
 BLDCDriver3PWM driver = BLDCDriver3PWM(GPIO_NUM_19, GPIO_NUM_18, GPIO_NUM_5, GPIO_NUM_17);
 
+//target variable
+float target_velocity = 0;
+
 // instantiate the commander
 Commander command = Commander(Serial);
-void doTarget(char* cmd) { command.scalar(&motor.target, cmd); }
+void doTarget(char* cmd) { command.scalar(&target_velocity, cmd); }
+void doLimit(char* cmd) { command.scalar(&motor.voltage_limit, cmd); }
 
 void setup() {
 
   // driver config
   // power supply voltage [V]
   driver.voltage_power_supply = 12;
+  // limit the maximal dc voltage the driver can set
+  // as a protection measure for the low-resistance motors
+  // this value is fixed on startup
+  driver.voltage_limit = 6;
   driver.init();
   // link the motor and the driver
   motor.linkDriver(&driver);
 
   // limiting motor movements
-  // motor.phase_resistance = 3.52 // [Ohm]
-  // motor.current_limit = 2;   // [Amps] - if phase resistance defined
-  motor.voltage_limit = 3;   // [V] - if phase resistance not defined
-  motor.velocity_limit = 5; // [rad/s] cca 50rpm
+  // limit the voltage to be set to the motor
+  // start very low for high resistance motors
+  // current = voltage / resistance, so try to be well under 1Amp
+  motor.voltage_limit = 3;   // [V]
  
   // open loop control config
   motor.controller = MotionControlType::velocity_openloop;
@@ -34,17 +42,20 @@ void setup() {
 
   // add target command T
   command.add('T', doTarget, "target velocity");
+  command.add('L', doLimit, "voltage limit");
 
   Serial.begin(115200);
   Serial.println("Motor ready!");
   Serial.println("Set target velocity [rad/s]");
   _delay(1000);
 }
+
 void loop() {
 
   // open loop velocity movement
   // using motor.voltage_limit and motor.velocity_limit
-  motor.move();
+  // to turn the motor "backwards", just set a negative target_velocity
+  motor.move(target_velocity);
 
   // user communication
   command.run();
